06095b4382646de9fdd597451f297563dd84e08a
ebutuoy135 <43425377+ebutuoy135@users.noreply.github.com>
2018-10-10 12:36:55 -0400

    Add files via upload

---
+++ GuessMe.cli/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GuessMe.cli
+{
+    class Program
+    {
+        private static string name;
+        
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.Write("Hello " + name + "!");
+            Console.ReadKey();
+        }
+            
+             
+    }
+}
